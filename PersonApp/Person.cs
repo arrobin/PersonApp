82446db4250df1d0c1fec32833b59b61d7174afd
arrobin <ashiqur007@yahoo.com>
2015-05-12 19:36:21 +0600

    Person Application for understanding OOP

---
+++ PersonApp/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApp
+{
+    class Person
+    {
+        public string FirstName;
+        public string MiddleName;
+        public string LastName;
+
+        public string GetFullName()
+        {
+            return FirstName+" "+MiddleName+" "+LastName;
+        }
+
+        public string GetReverseFullName()
+        {
+            string fullName = GetFullName();
+            char[] cArray = fullName.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+        }
+    }
+}
---
+++ PersonApp/PersonUI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonApp
+{
+    public partial class PersonUI : Form
+    {
+        public PersonUI()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            Person aPerson1 = new Person();
+            aPerson1.FirstName = firstNameTextBox.Text;
+            aPerson1.MiddleName = middleNameTextBox.Text;
+            aPerson1.LastName = lastNameTextBox.Text;
+
+            string fullName = aPerson1.GetFullName();
+            string reverseName = aPerson1.GetReverseFullName();
+
+            fullNameTextBox.Text = fullName;
+            reverseNameTextBox.Text = reverseName;
+        }
+    }
+}
